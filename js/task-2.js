64e1746045f0da6f6ddb743c1bc43971473a3f86
Timoha <moytimoha2000@gmail.com>
2023-10-20 17:00:40 +0200

    bpvtytybz

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,5 @@
 `use stritt`
-function getShippingMessage(country, price, deliveryFee)
-
-return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+function getShippingMessage(country, price, deliveryFee) { return `Shipping to ${country} will cost ${price + deliveryFee} credits`; }
 
 console.log(getShippingMessage("Australia", 120, 58)); // "Shipping to Australia will cost 178 credits"
 console.log(getShippingMessage("Germany", 88, 20)); // "Shipping to Germany will cost 108 credits"
